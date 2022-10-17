2e46698e8e928ff5f34b9693e173c75324dd735d
psriley <psriley@ksu.edu>
2022-10-17 13:22:22 -0500

    Get matrix printed

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+def print_matrix(numList):
+    row = []
+    for cell in numList:
+        if cell is not ';':
+            row.append(cell)
+        if cell is ';' or numList.index(cell) is len(numList) - 1:
+            print(row)
+            row.clear()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_matrix(['4', '3', ';', '2', '1'])
